08de32ec6ae27d18fdd98cfa885a78a73d5e5071
nicolau.ed <nicolau.ed@coditramuntana.com>
2020-02-18 12:33:13 +0100

    basic horizontal calendar layout created, separated into files

---
+++ app/helpers/ct_calendar_for/horizontal_calendar_helper.rb
@@ -0,0 +1,82 @@
+module CtCalendarFor
+  module HorizontalCalendarHelper
+    require 'uri'
+
+    def horizontal_calendar options
+      @users = options[:users]
+      @start_date = options[:start_date] || Date.current
+      @end_date = options[:end_date] || @start_date.since(1.month)
+      @months_span = @end_date.to_date.month - @start_date.to_date.month
+      content_tag :div, class: "row" do
+        content_tag :div, class: "col-12" do
+          content_tag :table, class: "horizontal_table" do
+            concat horizontal_header options
+            concat horizontal_body options
+            concat user_lines
+          end
+        end
+      end
+    end
+
+    def horizontal_header options
+      content_tag :tr do
+        concat content_tag :th, nil, {colspan: 10, class: "empty-cell"}
+        for i in 0..@months_span
+          month_name = l(@start_date.to_date.beginning_of_month.since(i.months), format: '%B')
+          month_days = if @months_span.zero?
+            (@end_date.to_date - @start_date.to_date).to_i
+          elsif i == @months_span
+            (@end_date.to_date - @end_date.to_date.beginning_of_month).to_i
+          elsif i.zero?
+            (@start_date.to_date.end_of_month - @start_date.to_date).to_i
+          else
+            (@start_date.to_date.since(i.months).end_of_month - @start_date.to_date.since(i.months).beginning_of_month).to_i
+          end
+          concat content_tag  :th,
+                              month_name,
+                              {colspan: month_days, class: ""}
+        end
+      end
+    end
+
+    def horizontal_body options
+      options[:search_attribute] ||= "name"
+      options[:search_placeholder] ||= "Search"
+      content_tag :tr, class: "highlighted-row" do
+        concat content_tag :th, search_form_cell(options), {colspan: 10, class: "empty-cell"}
+        (@start_date.to_date..@end_date.to_date).each do |date|
+          concat content_tag :th, date.day, class: "text-center"
+        end
+      end
+    end
+
+    def search_form_cell options
+      if !options[:search?].nil? && options[:search?]
+        return search_form_for options[:object], url: options[:url] do |f|
+          content_tag :div, class: "row" do
+            content_tag :div, class: "col-12 user-search-wrapper" do
+              concat f.search_field "#{options[:search_attribute]}_cont".to_sym, :class => "form-control search-field rounded-search-field", placeholder: options[:search_placeholder]
+              concat button_tag type: "submit", class: "btn special-search-btn" do
+                content_tag :i, nil, class: "fa fa-search"
+              end
+            end
+          end
+        end
+      end
+    end
+
+    def user_lines
+      return nil if @users.nil?
+      @users.each do |user|
+        content_tag :tr do
+          concat content_tag :td, user.name, {colspan: 10, class: "cell-name pl-3 #{'corner-bottom-left-radius' if user == @users.last}"}
+          (@start_date.to_date..@end_date.to_date).each do |date|
+            concat content_tag :td, class: "" do
+              content_tag(:div, nil,class: "hz-calendar-ball")
+            end
+          end
+        end
+      end
+    end
+  end
+end
--- lib/ct_calendar_for/engine.rb
+++ lib/ct_calendar_for/engine.rb
@@ -1,32 +1,8 @@
 module CtCalendarFor
   class << self
     mattr_accessor :table_for_default_class
-    mattr_accessor :table_for_wrapper_default_class
-    mattr_accessor :table_for_default_action_base_class
-    mattr_accessor :table_for_action_class
-    mattr_accessor :table_for_breakpoint
-    mattr_accessor :table_for_icon_font_base_class
-    mattr_accessor :table_for_action_icons
-    mattr_accessor :table_for_numeric_percentage_precision
-    mattr_accessor :table_for_cell_for_image_image_class
-    mattr_accessor :table_for_truncate_length
-    mattr_accessor :table_for_truncate_separator
-    mattr_accessor :table_for_truncate_omission
-    mattr_accessor :table_for_td_default_prefix_class
 
-    self.table_for_wrapper_default_class = "table-responsive"
-    self.table_for_default_class = "table table-striped table-bordered table-condensed table-hover"
-    self.table_for_default_action_base_class = "btn btn-sm"
-    self.table_for_action_class = {show: "btn-primary", edit: "btn-success", destroy: "btn-danger", other: "btn-default"}
-    self.table_for_breakpoint = "992px"
-    self.table_for_icon_font_base_class = "fa"
-    self.table_for_action_icons = {show: "eye", edit: "pencil", destroy: "trash", custom: "gear"}
-    self.table_for_numeric_percentage_precision = 2
-    self.table_for_cell_for_image_image_class = "img-responsive"
-    self.table_for_truncate_length = 50
-    self.table_for_truncate_separator = " "
-    self.table_for_truncate_omission = "..."
-    self.table_for_td_default_prefix_class = "td-item"
+    self.table_for_default_class = ""
   end
 
   # this function maps the vars from your app into your engine
