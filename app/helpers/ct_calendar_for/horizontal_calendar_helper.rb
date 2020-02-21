fdeba07f6cc29331f589c92de410a3404e241cb5
nicolau.ed <nicolau.ed@coditramuntana.com>
2020-02-21 11:30:58 +0100

    Styling touches and minor corrections, users checked.

--- app/helpers/ct_calendar_for/horizontal_calendar_helper.rb
+++ app/helpers/ct_calendar_for/horizontal_calendar_helper.rb
@@ -9,28 +9,30 @@ module CtCalendarFor
       @months_span = @end_date.to_date.month - @start_date.to_date.month
       content_tag :div, class: "row" do
         content_tag :div, class: "col-12" do
-          content_tag :table, class: "horizontal_table" do
+          content_tag :table, class: "horizontal_table table table-bordered bg-white table-sm" do
             concat horizontal_header options
             concat horizontal_body options
-            concat user_lines
+            @users.each do |user|
+              concat user_lines user
+            end
           end
         end
       end
     end
 
     def horizontal_header options
-      content_tag :tr do
+      content_tag :tr, class: "table-dark text-dark" do
         concat content_tag :th, nil, {colspan: 10, class: "empty-cell"}
         for i in 0..@months_span
           month_name = l(@start_date.to_date.beginning_of_month.since(i.months), format: '%B')
           month_days = if @months_span.zero?
-            (@end_date.to_date - @start_date.to_date).to_i
+            (@end_date.to_date - @start_date.to_date).to_i + 1
           elsif i == @months_span
-            (@end_date.to_date - @end_date.to_date.beginning_of_month).to_i
+            (@end_date.to_date - @end_date.to_date.beginning_of_month).to_i + 1
           elsif i.zero?
-            (@start_date.to_date.end_of_month - @start_date.to_date).to_i
+            (@start_date.to_date.end_of_month - @start_date.to_date).to_i + 1
           else
-            (@start_date.to_date.since(i.months).end_of_month - @start_date.to_date.since(i.months).beginning_of_month).to_i
+            (@start_date.to_date.since(i.months).end_of_month - @start_date.to_date.since(i.months).beginning_of_month).to_i + 1
           end
           concat content_tag  :th,
                               month_name,
@@ -42,7 +44,7 @@ module CtCalendarFor
     def horizontal_body options
       options[:search_attribute] ||= "name"
       options[:search_placeholder] ||= "Search"
-      content_tag :tr, class: "highlighted-row" do
+      content_tag :tr, class: "table-dark text-dark" do
         concat content_tag :th, search_form_cell(options), {colspan: 10, class: "empty-cell"}
         (@start_date.to_date..@end_date.to_date).each do |date|
           concat content_tag :th, date.day, class: "text-center"
@@ -65,16 +67,11 @@ module CtCalendarFor
       end
     end
 
-    def user_lines
-      return nil if @users.nil?
-      @users.each do |user|
-        content_tag :tr do
-          concat content_tag :td, user.name, {colspan: 10, class: "cell-name pl-3 #{'corner-bottom-left-radius' if user == @users.last}"}
-          (@start_date.to_date..@end_date.to_date).each do |date|
-            concat content_tag :td, class: "" do
-              content_tag(:div, nil,class: "hz-calendar-ball")
-            end
-          end
+    def user_lines user
+      return content_tag :tr do
+        concat content_tag :td, user.name, {colspan: 10, class: "cell-name pl-3 table-active border-dark"}
+        (@start_date.to_date..@end_date.to_date).each do |date|
+          concat content_tag :td, content_tag(:div, nil,class: "hz-calendar-ball"), class: ""
         end
       end
     end
